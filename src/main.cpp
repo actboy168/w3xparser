0bd14ab3a4f1211f24517b5af91861d201e3e4bd
actboy168 <actboy168@gmail.com>
2019-02-22 11:28:15 +0800

    Revert "Update main.cpp"
    This reverts commit a9004b4616850d4a5904cea0f2ba0ffd90db8729.

--- src/main.cpp
+++ src/main.cpp
@@ -44,7 +44,7 @@ namespace w3x
 				}
 				lua_pushlstring(L, l.row[x].data(), l.row[x].size());
 				auto& data = l.data[x][y];
-				if (is_string(data) || !data.empty()) {
+				if (!data.empty()) {
 					lua_pushlstring(L, data.data(), data.size());
 				}
 				else {
